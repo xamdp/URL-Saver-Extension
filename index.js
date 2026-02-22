946ca310cffee82a1dd28a6a8f2903cb39cee63e
xamdp <64017084+xamdp@users.noreply.github.com>
2026-02-22 23:07:46 +0900

    add function to render links from input field
    used a temporary way to render links and opens to new tab.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 
+mylinks = []
 
-let inputBtn = document.getElementById('input-btn')
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener("click", function() {
-	console.log("Button clicked from addEventListener")
+	mylinks.push(inputEl.value);
+	inputEl.value = '';
+	renderLinks()
+
 })
+
+function renderLinks() {
+	let listItems = ''
+	for (let link = 0; link < mylinks.length; link++) {
+		listItems += "<li>" + "<a target='_blank' rel='noopener noreferrer' href='" + mylinks[link] + "'>"+ mylinks[link] + "</a>"+ "</li>"
+	}
+	ulEl.innerHTML = listItems
+}
+
+
