8e6f03e94907b32a5e3f1d56b2d862ba553e4715
xamdp <64017084+xamdp@users.noreply.github.com>
2026-04-29 18:01:52 +0900

    Format whitespaces

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+mylinks = [];
 
-mylinks = []
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
 
-const inputEl = document.getElementById('input-el')
-const inputBtn = document.getElementById('input-btn')
-const ulEl = document.getElementById('ul-el')
-
-inputBtn.addEventListener("click", function() {
-	mylinks.push(inputEl.value);
-	inputEl.value = '';
-	renderLinks()
-
-})
+inputBtn.addEventListener('click', function () {
+    mylinks.push(inputEl.value);
+    inputEl.value = '';
+    renderLinks();
+});
 
 function renderLinks() {
-	let listItems = ''
-	for (let link = 0; link < mylinks.length; link++) {
-		listItems += "<li>" + "<a target='_blank' rel='noopener noreferrer' href='" + mylinks[link] + "'>"+ mylinks[link] + "</a>"+ "</li>"
-	}
-	ulEl.innerHTML = listItems
+    let listItems = '';
+    for (let link = 0; link < mylinks.length; link++) {
+        listItems += `<li><a target="_blank" rel="noopener noreferrer" href="${mylinks[link]}"> ${mylinks[link]}</a></li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
 
-
+function here() {
+    for (let i = 0; i < mylinks.length; i++) {
+        console.log('just something');
+    }
+}
